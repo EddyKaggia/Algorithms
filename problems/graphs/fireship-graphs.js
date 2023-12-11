cf3bffbd409c455b7674bfc3019c51e9ee735795
EddyKaggia <kagzcodes@gmail.com>
2023-12-11 14:14:42 -0800

    feat: graphs

--- problems/graphs/fireship-graphs.js
+++ problems/graphs/fireship-graphs.js
@@ -33,9 +33,49 @@ function addEdge(origin, destination) {
 airports.forEach(addNode);
 routes.forEach((route) => addEdge(...route));
 
-// BFS
+// BFS (Would find all destinations)
 function bfs(start) {
+  const visited = new Set();
+
   const queue = [start];
+
+  while (queue.length > 0) {
+    const airport = queue.shift(); // mutates the queue
+
+    const destinations = adjacencyList.get(airport);
+
+    for (const destination of destinations) {
+      if (destination === "BKK") {
+        return `BFS found Bangkok!`;
+      }
+
+      if (!visited.has(destination)) {
+        visited.add(destination);
+        queue.push(destination);
+      }
+    }
+  }
 }
 
-//DFS
+//DFS (Would find quickest route from PHX -> BKK)
+function dfs(start, visited = new Set()) {
+  console.log(start);
+
+  visited.add(start);
+
+  const destinations = adjacencyList.get(start);
+
+  for (const destination of destinations) {
+    if (destination === "BKK") {
+      console.log(`DFS found Bangkok`);
+      return;
+    }
+
+    if (!visited.has(destination)) {
+      dfs(destination, visited);
+    }
+  }
+}
+
+console.log(bfs("PHX"));
+console.log(dfs("PHX"));
