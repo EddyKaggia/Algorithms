829a4e86335d891a05e0c6d75dd309086001e457
EddyKaggia <kagzcodes@gmail.com>
2024-03-10 11:28:38 -0700

    feat: Graphs isConnected adjacency matrix

--- problems/graphs/coderbyte-graphs.js
+++ problems/graphs/coderbyte-graphs.js
@@ -2,7 +2,7 @@
 How do we represent a graph in code?
 */
 
-// Vertex list + Edge list
+// VERTEX LIST list + EDGE LIST
 
 //TC to find adjacent nodes -> O(e) where e is teh number of edges
 //TC to check if two nodes are connected -> O(e)
@@ -46,7 +46,9 @@ const findAdjacentNodes = function (node) {
   return adjacentNodes;
 };
 
-console.log(findAdjacentNodes("E"));
+console.log(findAdjacentNodes("A"));
+
+// 2) ARE NODES CONNECTED
 
 const isConnected = function (node1, node2) {
   for (const [first, second] of edges) {
@@ -60,11 +62,23 @@ const isConnected = function (node1, node2) {
   return false;
 };
 
-console.log(isConnected("C", "A"));
+console.log(isConnected("D", "B"));
 
-// Adjacency Matrix
+// ADJACENCY MATRIX
+
+// TC to find adjacent nodes: O(v) where v is the number of vertices
+// TC to check if two nodes are connected: O(1)
+// SC: O(V^2) where v is the number of vertices
 
 const vertices2 = ["A", "B", "C", "D", "E"];
+
+const verticesIdx = {
+  A: 0,
+  B: 1,
+  C: 2,
+  D: 3,
+  E: 4,
+};
 
 const adjacencyMatrix = [
   [0, 1, 0, 1, 0],
@@ -73,3 +87,39 @@ const adjacencyMatrix = [
   [1, 0, 1, 0, 1],
   [0, 0, 1, 1, 0],
 ];
+
+// 1) FIND ADJACENT NODES
+
+const findAdjacentNodes2 = (node) => {
+  const adjacentNodes = [];
+
+  // Iterate through vertices array
+  for (let i = 0; i < vertices2.length; i++) {
+    nodeVertex = verticesIdx[node];
+
+    if (adjacencyMatrix[nodeVertex][i] === 1) {
+      adjacentNodes.push(vertices2[i]);
+    }
+  }
+
+  //Iterate through row, and check if any node = 1, if yes, push into output array
+
+  return adjacentNodes;
+};
+
+console.log(findAdjacentNodes2("A")); // ["B", "D"]
+console.log(findAdjacentNodes2("C")); // ["B", "D", "E"]
+
+// 2) ARE NODES CONNECTED
+
+const isConnected2 = (node1, node2) => {
+  const nodeIdx1 = verticesIdx[node1];
+  const nodeIdx2 = verticesIdx[node2];
+
+  return !!adjacencyMatrix[nodeIdx1][nodeIdx2];
+};
+
+console.log(isConnected2("A", "B")); //True
+console.log(isConnected2("A", "E")); //False
+console.log(isConnected2("C", "D")); //True
+console.log(isConnected2("C", "A")); //False
