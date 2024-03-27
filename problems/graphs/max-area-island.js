0d835a7ff9103b75f37f0ddea4a35cb43fde5832
EddyKaggia <kagzcodes@gmail.com>
2024-03-27 13:49:36 -0700

    feat: leetcode 695 - Max area of island graph problem

---
+++ problems/graphs/max-area-island.js
@@ -0,0 +1,68 @@
+/*
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+The area of an island is the number of cells with a value 1 in the island.
+
+Return the maximum area of an island in grid. If there is no island, return 0.
+
+Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+Example 2:
+
+Input: grid = [[0,0,0,0,0,0,0,0]]
+Output: 0
+*/
+
+const maxAreaOfIsland = (grid) => {
+  if (!grid || grid.length === 0) return;
+
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let maxArea = 0;
+
+  const dfs = (row, col) => {
+    if (
+      row < 0 ||
+      row >= rows ||
+      col < 0 ||
+      col >= cols ||
+      grid[row][col] === 0
+    )
+      return 0;
+
+    grid[row][col] = 0;
+
+    let area = 1;
+
+    area += dfs(row - 1, col);
+    area += dfs(row + 1, col);
+    area += dfs(row, col - 1);
+    area += dfs(row, col + 1);
+
+    return area;
+  };
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === 1) {
+        maxArea = Math.max(maxArea, dfs(i, j));
+      }
+    }
+  }
+
+  return maxArea;
+};
+
+console.log(
+  maxAreaOfIsland([
+    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+    [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+  ])
+); // 6
